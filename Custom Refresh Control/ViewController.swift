f2b5be3d73482907de9d5d1367581858af688b8a
Zedd <zedd.0202@kakaocorp.com>
2019-04-21 15:19:22 +0900

    default refresh control

--- Custom Refresh Control/ViewController.swift
+++ Custom Refresh Control/ViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.setupTableView()
+        self.configureRefreshControl()
+    }
+    
+    func setupTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    func configureRefreshControl () {
+        // Add the refresh control to your UIScrollView object.
+        self.tableView.refreshControl = UIRefreshControl()
+        self.tableView.refreshControl?.addTarget(self, action:
+            #selector(handleRefreshControl),
+                                                  for: .valueChanged)
+    }
+    
+    @objc func handleRefreshControl() {
+        // Update your content…
+        
+        // Dismiss the refresh control.
+        DispatchQueue.main.async {
+            self.tableView.refreshControl?.endRefreshing()
+        }
     }
 
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell"
+            , for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
 
 }
 
